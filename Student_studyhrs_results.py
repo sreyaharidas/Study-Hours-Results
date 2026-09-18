9737154c5031f0369f8ccaf0fc9e026dbb7c10f4
sreyaharidas <151733059+sreyaharidas@users.noreply.github.com>
2026-09-18 12:11:33 +0530

    Refactor demo.launch to run in main block

--- Student_studyhrs_results.py
+++ Student_studyhrs_results.py
@@ -42,7 +42,9 @@ demo = gr.Interface(
 
 import os
 
-demo.launch(
-    server_name="0.0.0.0",
-    server_port=int(os.environ.get("PORT", 7860))
-)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 7860))
+    demo.launch(
+        server_name="0.0.0.0",
+        server_port=port
+    )
